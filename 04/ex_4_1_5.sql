7c148b88320c9506483ba0588e7cc6e574fc4956
Sebastião Teixeira <sebastiao.teixeira@ua.pt>
2024-03-17 16:14:31 +0000

    Fix many-to-many relations in Conferencias system

--- 04/ex_4_1_5.sql
+++ 04/ex_4_1_5.sql
@@ -1,9 +1,15 @@
+IF OBJECT_ID('Conferencias_ArtigoCientifico_Conferencia', 'U') IS NOT NULL
+    DROP TABLE dbo.Conferencias_ArtigoCientifico_Conferencia
+IF OBJECT_ID('Conferencias_Participante_Conferencia', 'U') IS NOT NULL
+    DROP TABLE dbo.Conferencias_Participante_Conferencia
 IF OBJECT_ID('Conferencias_NaoEstudante', 'U') IS NOT NULL
     DROP TABLE dbo.Conferencias_NaoEstudante
 IF OBJECT_ID('Conferencias_Estudante', 'U') IS NOT NULL
     DROP TABLE dbo.Conferencias_Estudante
 IF OBJECT_ID('Conferencias_Participante', 'U') IS NOT NULL
     DROP TABLE dbo.Conferencias_Participante
+IF OBJECT_ID('Conferencias_ArtigoCientifico_Autor', 'U') IS NOT NULL
+    DROP TABLE dbo.Conferencias_ArtigoCientifico_Autor
 IF OBJECT_ID('Conferencias_Autor', 'U') IS NOT NULL
     DROP TABLE dbo.Conferencias_Autor
 IF OBJECT_ID('Conferencias_ArtigoCientifico', 'U') IS NOT NULL
@@ -31,10 +37,16 @@ CREATE TABLE Conferencias_Autor (
     Pessoa_enderecoEmail VARCHAR(255),
     Pessoa_nome VARCHAR(255),
     instituicao VARCHAR(255),
+    PRIMARY KEY (Pessoa_enderecoEmail),
+    FOREIGN KEY (Pessoa_enderecoEmail) REFERENCES Conferencias_Pessoa(enderecoEmail)
+);
+
+CREATE TABLE Conferencias_ArtigoCientifico_Autor (
     ArtigoCientifico_numRegistro INT,
-    PRIMARY KEY (Pessoa_enderecoEmail, ArtigoCientifico_numRegistro),
-    FOREIGN KEY (Pessoa_enderecoEmail) REFERENCES Conferencias_Pessoa(enderecoEmail),
-    FOREIGN KEY (ArtigoCientifico_numRegistro) REFERENCES Conferencias_ArtigoCientifico(numRegistro)
+    Autor_Pessoa_enderecoEmail VARCHAR(255),
+    PRIMARY KEY (ArtigoCientifico_numRegistro, Autor_Pessoa_enderecoEmail),
+    FOREIGN KEY (ArtigoCientifico_numRegistro) REFERENCES Conferencias_ArtigoCientifico(numRegistro),
+    FOREIGN KEY (Autor_Pessoa_enderecoEmail) REFERENCES Conferencias_Autor(Pessoa_enderecoEmail)
 );
 
 CREATE TABLE Conferencias_Participante (
@@ -59,4 +71,20 @@ CREATE TABLE Conferencias_NaoEstudante (
     referenciaTransacao VARCHAR(255),
     PRIMARY KEY (Participante_Pessoa_enderecoEmail),
     FOREIGN KEY (Participante_Pessoa_enderecoEmail) REFERENCES Conferencias_Participante(Pessoa_enderecoEmail)
+);
+
+CREATE TABLE Conferencias_Participante_Conferencia (
+    Participante_Pessoa_enderecoEmail VARCHAR(255),
+    Conferencia_nome VARCHAR(255),
+    PRIMARY KEY (Participante_Pessoa_enderecoEmail, Conferencia_nome),
+    FOREIGN KEY (Participante_Pessoa_enderecoEmail) REFERENCES Conferencias_Participante(Pessoa_enderecoEmail),
+    FOREIGN KEY (Conferencia_nome) REFERENCES Conferencias_Conferencia(nome)
+);
+
+CREATE TABLE Conferencias_ArtigoCientifico_Conferencia (
+    ArtigoCientifico_numRegistro INT,
+    Conferencia_nome VARCHAR(255),
+    PRIMARY KEY (ArtigoCientifico_numRegistro, Conferencia_nome),
+    FOREIGN KEY (ArtigoCientifico_numRegistro) REFERENCES Conferencias_ArtigoCientifico(numRegistro),
+    FOREIGN KEY (Conferencia_nome) REFERENCES Conferencias_Conferencia(nome)
 );
