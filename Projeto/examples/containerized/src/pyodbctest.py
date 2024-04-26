d932021ce065b30c960164a37e7af5804c1786c3
Sebastião Teixeira <sebastiao.teixeira@ua.pt>
2024-04-26 12:48:52 +0100

    Add containerized version for odbc

---
+++ Projeto/examples/containerized/src/pyodbctest.py
@@ -0,0 +1,27 @@
+"""
+Connects to a SQL database using pyodbc
+"""
+import pyodbc
+import os
+
+SERVER = os.environ.get('SQL_SERVER')
+DATABASE = os.environ.get('SQL_DATABASE')
+USERNAME = os.environ.get('SQL_USER')
+PASSWORD = os.environ.get('SQL_PASSWORD')
+
+connectionString = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD};TrustServerCertificate=yes'
+
+conn = pyodbc.connect(connectionString)
+
+SQL_QUERY = """SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = N'ATL_Pessoa'"""
+
+
+cursor = conn.cursor()
+cursor.execute(SQL_QUERY)
+
+# Fetch all results
+for row in cursor:
+    print(row)
+
