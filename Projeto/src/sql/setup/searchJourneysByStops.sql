0a0d7960829e1c81e93ea95d37cd5969966699a8
Sebastião Teixeira <sebastiao.teixeira@ua.pt>
2024-05-18 11:08:41 +0100

    Add searchJourneysByStops function

---
+++ Projeto/src/sql/setup/searchJourneysByStops.sql
@@ -0,0 +1,38 @@
+-- Select all journeys such as its line has all the stops in @includedStops
+-- But no one is in exceptions table
+
+CREATE OR ALTER FUNCTION searchJourneysByStops
+(
+	@includedStops VARCHAR(1000)
+)
+RETURNS TABLE
+AS
+RETURN
+(
+	WITH SplitStops AS 
+	(
+		SELECT value AS id
+		FROM STRING_SPLIT(@includedStops, ',')
+	)
+	, StopCount AS
+	(
+		SELECT COUNT(*) AS count
+		FROM SplitStops
+	)
+
+	SELECT j.[id], j.[idLine], j.[idFirstStop], j.[idLastStop], j.[startTime], j.[outbound]
+	FROM [UrbanBus].[journey] AS j
+	WHERE NOT EXISTS 
+	(
+		SELECT 1
+		FROM [UrbanBus].[exceptions] AS e
+		WHERE e.idJourney = j.id
+	)
+	AND 
+	(
+		SELECT COUNT(*)
+		FROM [UrbanBus].[line_stop] AS ls
+		WHERE ls.idLine = j.idLine
+		AND ls.idStop IN (SELECT id FROM SplitStops)
+	) = (SELECT count FROM StopCount)
+);
