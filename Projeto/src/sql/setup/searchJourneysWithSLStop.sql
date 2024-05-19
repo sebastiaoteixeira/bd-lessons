5c63f0a72d869bb947c201ed0f225d7889ad4fff
Sebastião Teixeira <sebastiao.teixeira@ua.pt>
2024-05-19 11:43:19 +0100

    Fix infinite loop

--- Projeto/src/sql/setup/searchJourneysWithSLStop.sql
+++ Projeto/src/sql/setup/searchJourneysWithSLStop.sql
@@ -28,47 +28,47 @@ BEGIN
 		RETURN;
 	
 	-- If not, for all the results, remove if the stops are not in the same order
-	--DECLARE @journey INT;
-	--DECLARE @line INT;
-	--DECLARE @first INT;
-	--DECLARE @last INT;
-	--DECLARE @outbound BIT;
-	--DECLARE @time TIME;
-	--DECLARE @nextStop INT;
-	--DECLARE @stop INT;
+	DECLARE @journey INT;
+	DECLARE @line INT;
+	DECLARE @firstS INT;
+	DECLARE @lastS INT;
+	DECLARE @outbound BIT;
+	DECLARE @time TIME;
+	DECLARE @nextStop INT;
+	DECLARE @stop INT;
 
-	--DECLARE journeyCursor CURSOR FOR
-	--SELECT [id], [idLine], [idFirstStop], [idLastStop], [startTime], [outbound]
-	--FROM @result;
+	DECLARE journeyCursor CURSOR FOR
+	SELECT [id], [idLine], [idFirstStop], [idLastStop], [startTime], [outbound]
+	FROM @result;
 
-	--OPEN journeyCursor;
-	--FETCH NEXT FROM journeyCursor INTO @journey, @line, @first, @last, @time, @outbound;
-	--
-	--WHILE @@FETCH_STATUS = 0
-	--	BEGIN
-	--		SET @stop = @firstStop;
-	--		WHILE @stop != @last
-	--			BEGIN
-	--				SELECT @nextStop = idNextStop, @time = DATEADD(SECOND, DATEDIFF(SECOND, 0, @time) + DATEDIFF(SECOND, 0, timeToNext), 0)
-	--				FROM [UrbanBus].[line_stop]
-	--				WHERE idLine = @line
-	--				AND idStop = @stop;
+	OPEN journeyCursor;
+	FETCH NEXT FROM journeyCursor INTO @journey, @line, @firstS, @lastS, @time, @outbound;
+	
+	WHILE @@FETCH_STATUS = 0
+		BEGIN
+			SET @stop = @firstStop;
+			WHILE @stop != @lastS
+				BEGIN
+					SELECT @nextStop = idNextStop, @time = DATEADD(SECOND, DATEDIFF(SECOND, 0, @time) + DATEDIFF(SECOND, 0, timeToNext), 0)
+					FROM [UrbanBus].[line_stop]
+					WHERE idLine = @line
+					AND idStop = @stop;
 
-	--				IF @nextStop = @lastStop
-	--					BREAK;
-	--				IF @nextStop IS NULL
-	--					BEGIN
-	--						DELETE FROM @result
-	--						WHERE [id] = @journey;
-	--						BREAK;
-	--					END;
-	--			END;
-	--			SET @stop = @nextStop;
-	--	END;
-	--	FETCH NEXT FROM journeyCursor INTO @journey, @line, @first, @last, @time, @outbound;
+					IF @nextStop = @lastStop
+						BREAK;
+					IF @nextStop IS NULL
+						BEGIN
+							DELETE FROM @result
+							WHERE [id] = @journey;
+							BREAK;
+						END;
+					SET @stop = @nextStop;
+				END;
+			FETCH NEXT FROM journeyCursor INTO @journey, @line, @firstS, @lastS, @time, @outbound;
+		END;
 
-	--CLOSE journeyCursor;
-	--DEALLOCATE journeyCursor;
+	CLOSE journeyCursor;
+	DEALLOCATE journeyCursor;
 
 	RETURN;
 END;
